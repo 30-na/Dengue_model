d7e6ed6c5f0a2e0333eab4d0723c50257a8a868c
sina mokhtar <sinamokhtar@gmail.com>
2022-10-08 15:18:07 -0500

    add quadratic and inverse precipitation model

--- Functions/function_load_R0.R
+++ Functions/function_load_R0.R
@@ -2,75 +2,102 @@
 library(dplyr)
 library(raster)
 
+runR0 = function(month, preModel, rdaName){
+    
+    #load data
+    load(paste("processedData/World/", month , ".RDA" , sep = ""))
+    load("processedData/aegypti.rda")
+    load("processedData/globalEconomic.rda")
+    
+    
+    # merge datasets and define the parameters
+    R0 = aegypti %>%
+        left_join(climate, by = c("x", "y")) %>%
+        left_join(globalEconomic, by = c("x", "y"))%>%
+        mutate(b = ifelse(t <= 13.35 | t >= 40.08,
+                          0,
+                          2.02*10^(-4)*t*(t-13.35)*sqrt(40.08-t)),
+               
+               B_vh = ifelse(t <= 17.05 | t >= 35.83,
+                             0,
+                             8.49*10^(-4)*t*(t-17.05)*sqrt(35.83-t)),
+               # beta_hv = 4.91E-04*T*(T-12.22)*(37.46-T)^0.5(corrected formula)
+               B_hv = ifelse(t <= 12.22 | t >= 37.46,
+                             0,
+                             4.91*10^(-04)*t*(t-12.22)*sqrt(37.46-t)),
+               
+               sigma_v = ifelse(t <= 18.27 | t >= 42.31,
+                                0,
+                                1.74*10^(-4)*t*(t-18.27)*sqrt(42.31-t)),
+               
+               theta = ifelse(t <= 14.58 | t >= 34.61,
+                              0,
+                              8.56*10^(-3)*t*(t-14.58)*sqrt(34.61-t)),
+               
+               nu = ifelse(t <= 13.56 | t >= 38.29,
+                           0,
+                           -5.99*10^(-3)*(t-38.29)*(t-13.56)),
+               
+               pi = ifelse(t <= 11.36 | t >= 39.17,
+                           0,
+                           7.86*10^(-5)*t*(t-11.36)*sqrt(39.17-t)),
+               
+               mu = ifelse(t <= 11.25 | t >= 37.22,
+                           Inf,
+                           1/(-3.02*10^(-1)*(t-11.25)*(t-37.22))),
+               gamma = 1/5,
+               K = 20,
+               N_h = 1,
+               c_briere = 7.86*10^(-5),
+               z_briere = .05,
+               c_quad = -5.99*10^(-3),
+               z_quad = 0.025,
+               z_inverse = 0.6,
+               # 246 corrected value
+               FR_briere = ifelse(r > 246 | r < 0,
+                                  0,
+                                  c_briere*r*(r-0)*sqrt(246-r)*z_briere),
+               
+               FR_quad = ifelse(r < 0 | r > 123,
+                                0,
+                                c_quad*(r-1)*(r-123)*z_quad),
+               
+               FR_inverse = 1/r*z_inverse,
+               
+               r0_briere = ifelse((theta*nu*pi) < mu^2,
+                                  0,
+                                  sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_briere * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h))),
+               
+               r0_quadratic = ifelse((theta*nu*pi) < mu^2,
+                                     0,
+                                     sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_quad * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h))),
+               
+               r0_inverse = ifelse((theta*nu*pi) < mu^2,
+                                  0,
+                                  sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_inverse * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h))))
+               
+               #r0 = case_when((theta*nu*pi) < mu^2 ~ 0,
+               #               preModel == "briere" ~ sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_briere * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h)),
+               #               preModel == "quadratic" ~ sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_quad * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h)),
+               #               preModel == "inverese" ~ sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_inverse * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h))))
+    
+    
+    save(R0,
+         file = paste("processedData/", rdaName, ".rda", sep=""))
+    
+}
 
 
 month = "January"
-load(paste("processedData/World/", month , ".RDA" , sep = ""))
-load("processedData/aegypti.rda")
-load("processedData/globalEconomic.rda")
-
-
-# merge datasets
-R0 = aegypti %>%
-    left_join(climate, by = c("x", "y")) %>%
-    left_join(globalEconomic, by = c("x", "y"))%>%
-    mutate(b = ifelse(t <= 13.35 | t >= 40.08,
-                      0,
-                      2.02*10^(-4)*t*(t-13.35)*sqrt(40.08-t)),
-           
-           B_vh = ifelse(t <= 17.05 | t >= 35.83,
-                         0,
-                         8.49*10^(-4)*t*(t-17.05)*sqrt(35.83-t)),
-           # beta_hv = 4.91E-04*T*(T-12.22)*(37.46-T)^0.5(corrected formula)
-           B_hv = ifelse(t <= 12.22 | t >= 37.46,
-                         0,
-                         4.91*10^(-04)*t*(t-12.22)*sqrt(37.46-t)),
-           
-           sigma_v = ifelse(t <= 18.27 | t >= 42.31,
-                            0,
-                            1.74*10^(-4)*t*(t-18.27)*sqrt(42.31-t)),
-           
-           theta = ifelse(t <= 14.58 | t >= 34.61,
-                          0,
-                          8.56*10^(-3)*t*(t-14.58)*sqrt(34.61-t)),
-           
-           nu = ifelse(t <= 13.56 | t >= 38.29,
-                       0,
-                       -5.99*10^(-3)*(t-38.29)*(t-13.56)),
-           
-           pi = ifelse(t <= 11.36 | t >= 39.17,
-                       0,
-                       7.86*10^(-5)*t*(t-11.36)*sqrt(39.17-t)),
-           
-           mu = ifelse(t <= 11.25 | t >= 37.22,
-                       Inf,
-                       1/(-3.02*10^(-1)*(t-11.25)*(t-37.22))),
-           gamma = 1/5,
-           K = 1000,
-           N_h = 1,
-           c_briere = 7.86*10^(-5),
-           z_briere = .05,
-           c_quad = -5.99*10^(-3),
-           z_quad = 0.025,
-           # 246 corrected value
-           FR_briere = ifelse(r >= 246,
-                              0,
-                              c_briere*r*(r-0)*sqrt(246-r)*z_briere),
-           
-           r0 = ifelse((theta*nu*pi) < mu^2,
-                       0,
-                       sqrt((b^2 * B_vh * B_hv * sigma_v * R_se * K * FR_briere * P_ae * (1-(mu^2/(theta*nu*pi))))/(gamma * mu * (sigma_v+mu) * N_h))))
-           
-           
-save(R0,
-     file = "processedData/R0_january.rda")
-
-
-apply(R0, 2, range, na.rm=T)
+preModel = "quadratic"
+rdaName = "R0_January"
+runR0(month, preModel, rdaName)
 
 
 
+apply(R0, 2, quantile, na.rm=T)
 
+a = filter(R0, is.nan(r0))
 
 
 
--- Functions/plot.R
+++ Functions/plot.R
@@ -16,34 +16,3 @@ r0_map = ggplot() +
 ggsave("Figures/r0_January.jpg",
        r0_map,
        height=4,width=8,scale=1.65)
-######################
-load("processedData/R0fr_january.rda")
-r0_map = ggplot() +
-    geom_raster(data = R0 , aes(x = x,
-                                y = y,
-                                fill = r0)) +
-    scale_fill_viridis_c() +
-    coord_quickmap()+
-    labs(title ="Global Relative R0 (without square) Model for Dengue (January)",
-         x = "",
-         y = "")
-
-ggsave("Figures/r0_January.jpg",
-       r0_map,
-       height=4,width=8,scale=1.65)
-######################################
-
-load("processedData/fr_january.rda")
-fr_map = ggplot() +
-    geom_raster(data = fr , aes(x = x,
-                                y = y,
-                                fill = FR_briere)) +
-    scale_fill_viridis_c() +
-    coord_quickmap()+
-    labs(title ="F(R_briere) world map",
-         x = "",
-         y = "")
-
-ggsave("Figures/fr_January.jpg",
-       fr_map,
-       height=4,width=8,scale=1.65)

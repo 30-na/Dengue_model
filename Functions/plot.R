a32d3c0cc4f43ecb2d777a4b393487c2f2e7cc7e
sina mokhtar <sinamokhtar@gmail.com>
2022-10-08 16:46:10 -0500

    delete an empty line

--- Functions/plot.R
+++ Functions/plot.R
@@ -2,17 +2,19 @@
 library(ggplot2)
 
 
-load("processedData/R0_january.rda")
+load("processedData/R0_January.rda")
 r0_map = ggplot() +
     geom_raster(data = R0 , aes(x = x,
                                 y = y,
-                                fill = r0)) +
+                                fill = r0_inverse)) +
     scale_fill_viridis_c() +
     coord_quickmap()+
-    labs(title ="Global Relative R0 Model for Dengue (January)",
+    labs(title ="January Global Relative R0 Model for Dengue with inverse precipitation model",
          x = "",
-         y = "")
+         y = "")+
+    guides(fill = guide_colorbar(title = "R0 Number"))
+                 #guide_legend(title = "Basic Reproductive Number (R0)"))
 
-ggsave("Figures/r0_January.jpg",
+ggsave("Figures/r0_January_inverse.jpg",
        r0_map,
        height=4,width=8,scale=1.65)

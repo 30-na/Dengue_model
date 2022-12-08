afb61a698ef22fbbe3ffd8876344c1022d78adbc
sina mokhtar <sinamokhtar@gmail.com>
2022-12-07 18:53:33 -0600

    change scale and add brierer model

--- Functions/plot.R
+++ Functions/plot.R
@@ -126,7 +126,7 @@ saveMapMonth = function(month){
     
     ggsave(paste("Figures/without_FR/r0", month, "without_FR.jpg", sep="_"),
            r0_map,
-           height=4,width=8,scale=1.65)
+           height=4,width=8,scale=1)
     
     
     
@@ -161,7 +161,43 @@ saveMapMonth = function(month){
     
     ggsave(paste("Figures/inverse/r0", month, "inverse.jpg", sep="_"),
            r0_map,
-           height=4,width=8,scale=1.65)
+           height=4,width=8,scale=1)
+    
+    
+    
+    
+    
+    # Briere precipitation model
+    r0_map = ggplot() +
+        geom_raster(data = R0 , aes(x = x,
+                                    y = y,
+                                    fill = r0_briere)) +
+        scico::scale_fill_scico(palette = "lajolla")+
+        
+        labs(title =paste(month, " Global Relative R0 Model for Dengue",
+                          sep = ""),
+             x = "",
+             y = "",
+             subtitle = "Briere precipitation model")+
+        theme_minimal()+
+        theme(axis.title.x=element_blank(),
+              axis.title.y=element_blank(),
+              axis.ticks.x = element_blank(),
+              axis.text.x = element_blank(),
+              axis.ticks.y = element_blank(),
+              axis.text.y = element_blank(),
+              plot.background = element_blank(),
+              panel.grid.major = element_blank(),
+              panel.grid.minor = element_blank())+
+        guides(fill = guide_colorbar(title = "R0 Number"))+
+        geom_sf(data = spdf_world, fill = "transparent")+
+        coord_sf(xlim = c(min(R0$x), max(R0$x)), ylim = c(min(R0$y), max(R0$y)), expand = FALSE)
+    
+    
+    ggsave(paste("Figures/briere/r0", month, "briere.jpg", sep="_"),
+           r0_map,
+           height=4,width=8,scale=1)
+    
     
     
     
@@ -197,7 +233,7 @@ saveMapMonth = function(month){
     
     ggsave(paste("Figures/quadratic/r0", month, "quadratic.jpg", sep="_"),
            r0_map,
-           height=4,width=8,scale=1.65)
+           height=4,width=8,scale=1.0)
     
 }
 

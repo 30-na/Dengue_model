c2066e44c2c684081e9acd21afad11968c10b906
sina mokhtar <sinamokhtar@gmail.com>
2023-06-28 20:21:26 -0600

    add two different model for R0

--- Functions/plot_alt.R
+++ Functions/plot_alt.R
@@ -4,41 +4,8 @@ library(sf)
 library(rnaturalearth)
 
 saveMapMonth = function(month){
-    load(paste("processedData/R0_A_", month, ".rda", sep=""))
+    load(paste("processedData/R0_", month, ".rda", sep=""))
     spdf_world = rnaturalearth::ne_countries(returnclass="sf")
-    
-    
-    # Inverse precipitation model
-    r0_map = ggplot() +
-        geom_raster(data = R0 , aes(x = x,
-                                    y = y,
-                                    fill = r0_A_inverse)) +
-        scico::scale_fill_scico(palette = "lajolla")+
-        
-        labs(title =paste(month, " Global Relative R0 Model for Dengue",
-                          sep = ""),
-             x = "",
-             y = "",
-             subtitle = "Inverse precipitation model")+
-        theme_minimal()+
-        theme(axis.title.x=element_blank(),
-              axis.title.y=element_blank(),
-              axis.ticks.x = element_blank(),
-              axis.text.x = element_blank(),
-              axis.ticks.y = element_blank(),
-              axis.text.y = element_blank(),
-              plot.background = element_blank(),
-              panel.grid.major = element_blank(),
-              panel.grid.minor = element_blank())+
-        guides(fill = guide_colorbar(title = "R0 Number"))+
-        geom_sf(data = spdf_world, fill = "transparent")+
-        coord_sf(xlim = c(min(R0$x), max(R0$x)), ylim = c(min(R0$y), max(R0$y)), expand = FALSE)
-    
-    
-    ggsave(paste("Figures/inverse/r0_A_", month, "inverse.jpg", sep="_"),
-           r0_map,
-           height=4,width=8,scale=1)
-    
     
     
     
@@ -47,7 +14,7 @@ saveMapMonth = function(month){
     r0_map = ggplot() +
         geom_raster(data = R0 , aes(x = x,
                                     y = y,
-                                    fill = r0_A_briere)) +
+                                    fill = r0_briere)) +
         scico::scale_fill_scico(palette = "lajolla")+
         
         labs(title =paste(month, " Global Relative R0 Model for Dengue",
@@ -70,28 +37,25 @@ saveMapMonth = function(month){
         coord_sf(xlim = c(min(R0$x), max(R0$x)), ylim = c(min(R0$y), max(R0$y)), expand = FALSE)
     
     
-    ggsave(paste("Figures/briere/r0_A_", month, "briere.jpg", sep="_"),
+    ggsave(paste("Figures/briere/r0", month, "briere.jpg", sep="_"),
            r0_map,
            height=4,width=8,scale=1)
     
     
     
     
-    
-    
-    
-    # Quadratic precipitation model
-    r0_map = ggplot() +
+    # Briere precipitation model Alternative
+    r0_A_map = ggplot() +
         geom_raster(data = R0 , aes(x = x,
                                     y = y,
-                                    fill = r0_A_quadratic)) +
+                                    fill = r0_A_briere)) +
         scico::scale_fill_scico(palette = "lajolla")+
         
-        labs(title =paste(month, " Global Relative R0 Model for Dengue",
+        labs(title =paste(month, " Global Relative R0 Model for Dengue Alternative Model",
                           sep = ""),
              x = "",
              y = "",
-             subtitle = "Quadratic precipitation model")+
+             subtitle = "Briere precipitation model ")+
         theme_minimal()+
         theme(axis.title.x=element_blank(),
               axis.title.y=element_blank(),
@@ -107,11 +71,82 @@ saveMapMonth = function(month){
         coord_sf(xlim = c(min(R0$x), max(R0$x)), ylim = c(min(R0$y), max(R0$y)), expand = FALSE)
     
     
-    ggsave(paste("Figures/quadratic/r0_A_", month, "quadratic.jpg", sep="_"),
-           r0_map,
-           height=4,width=8,scale=1.0)
+    ggsave(paste("Figures/briere/r0_A_", month, "briere.jpg", sep="_"),
+           r0_A_map,
+           height=4,width=8,scale=1)
     
+
+
+
+# quadratic precipitation model
+r0_map_q = ggplot() +
+    geom_raster(data = R0 , aes(x = x,
+                                y = y,
+                                fill = r0_quadratic)) +
+    scico::scale_fill_scico(palette = "lajolla")+
+    
+    labs(title =paste(month, " Global Relative R0 Model for Dengue",
+                      sep = ""),
+         x = "",
+         y = "",
+         subtitle = "quadratic precipitation model")+
+    theme_minimal()+
+    theme(axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_blank(),
+          plot.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())+
+    guides(fill = guide_colorbar(title = "R0 Number"))+
+    geom_sf(data = spdf_world, fill = "transparent")+
+    coord_sf(xlim = c(min(R0$x), max(R0$x)), ylim = c(min(R0$y), max(R0$y)), expand = FALSE)
+
+
+ggsave(paste("Figures/quadratic/r0", month, "quadratic.jpg", sep="_"),
+       r0_map_q,
+       height=4,width=8,scale=1)
+
+
+
+
+# quadratic precipitation model Alternative
+r0_A_map_q = ggplot() +
+    geom_raster(data = R0 , aes(x = x,
+                                y = y,
+                                fill = r0_A_quadratic)) +
+    scico::scale_fill_scico(palette = "lajolla")+
+    
+    labs(title =paste(month, " Global Relative R0 Model for Dengue Alternative Model",
+                      sep = ""),
+         x = "",
+         y = "",
+         subtitle = "quadratic precipitation model ")+
+    theme_minimal()+
+    theme(axis.title.x=element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.text.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.y = element_blank(),
+          plot.background = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())+
+    guides(fill = guide_colorbar(title = "R0 Number"))+
+    geom_sf(data = spdf_world, fill = "transparent")+
+    coord_sf(xlim = c(min(R0$x), max(R0$x)), ylim = c(min(R0$y), max(R0$y)), expand = FALSE)
+
+
+ggsave(paste("Figures/quadratic/r0_A_", month, "quadratic.jpg", sep="_"),
+       r0_A_map_q,
+       height=4,width=8,scale=1)
+
+
 }
+
+
 
 
 saveMapMonth("January")

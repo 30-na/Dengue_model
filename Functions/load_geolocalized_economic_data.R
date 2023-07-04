cdb834b1e0fbb56b1a20a354a2daf6182c1f26dc
sina mokhtar <sinamokhtar@gmail.com>
2023-07-03 21:52:16 -0600

    replace G-Econ Data for 1990 for countries with G-Econ Na in year 2005

--- Functions/load_geolocalized_economic_data.R
+++ Functions/load_geolocalized_economic_data.R
@@ -23,26 +23,59 @@ if(!file.exists(destfile)){
 # read the file
 gEcon = read_excel("Data/Gecon40_post_final.xls",sheet = 1)
 
-# select coulmn and calculate risk of exposure
-lowest_ppp = gEcon %>%
-    dplyr::select(PPP2005_40, COUNTRY) %>%
-    group_by(COUNTRY)%>%
-    summarize(lowest = min(PPP2005_40)) %>%
-    filter(COUNTRY == "Djibouti")
+
+# Countries with G-Econ == NA in 2005
+countries_NA2005 = gEcon %>%
+    dplyr::filter(is.na(PPP2005_40)) %>%
+    dplyr::select(COUNTRY) %>%
+    distinct()
+
+
+a = gEcon %>%
+    filter(COUNTRY %in% countries_NA2005$COUNTRY) %>%
+    dplyr::select(COUNTRY,
+           PPP2005_40,
+           PPP2000_40,
+           PPP1995_40,
+           PPP1990_40
+           )
+
 
 PPP = gEcon %>%
-    dplyr::select(LAT, LONGITUDE, PPP2005_40, COUNTRY)%>%
-    # replace Somalia PPP2005_40 value with lowest Djibouti PPP2005_40
-    mutate(PPP2005_40 = if_else(COUNTRY == "Somalia" , lowest_ppp$lowest, PPP2005_40))%>%
-    
+    dplyr::select(LAT, LONGITUDE, PPP2005_40, COUNTRY, PPP1990_40) %>%
+    # replace G-Econ for 1990 if G-Econ value for 2005 is NA
+    mutate(PPP2005_40 = if_else(COUNTRY %in% countries_NA2005$COUNTRY, PPP1990_40, PPP2005_40)) %>%
     mutate(PPP_tranformed = log(PPP2005_40*exp(0.47))) %>%
     mutate(risk_exposure = case_when(PPP_tranformed < 1.97 ~ 1,
                                      PPP_tranformed > 4.911 ~ 0,
                                      PPP_tranformed > 1.97 & PPP_tranformed < 4.911 ~ (1.67-(0.34*PPP_tranformed)))) %>%
     dplyr::select("x" = LONGITUDE,
-           "y" = LAT,
-           "country" = COUNTRY,
-           "R_se" = risk_exposure)
+                  "y" = LAT,
+                  "country" = COUNTRY,
+                  "R_se" = risk_exposure)
+
+
+names(gEcon)
+# select coulmn and calculate risk of exposure
+# lowest_ppp = gEcon %>%
+#     dplyr::select(PPP2005_40, COUNTRY) %>%
+#     group_by(COUNTRY)%>%
+#     summarize(lowest = min(PPP2005_40)) %>%
+#     filter(COUNTRY == "Djibouti")
+
+# PPP = gEcon %>%
+#     dplyr::select(LAT, LONGITUDE, PPP2005_40, COUNTRY)%>%
+#     # replace Somalia PPP2005_40 value with lowest Djibouti PPP2005_40
+#     mutate(PPP2005_40 = if_else(COUNTRY == "Somalia" , lowest_ppp$lowest, PPP2005_40))%>%
+#     
+#     mutate(PPP_tranformed = log(PPP2005_40*exp(0.47))) %>%
+#     mutate(risk_exposure = case_when(PPP_tranformed < 1.97 ~ 1,
+#                                      PPP_tranformed > 4.911 ~ 0,
+#                                      PPP_tranformed > 1.97 & PPP_tranformed < 4.911 ~ (1.67-(0.34*PPP_tranformed)))) %>%
+#     dplyr::select("x" = LONGITUDE,
+#            "y" = LAT,
+#            "country" = COUNTRY,
+#            "R_se" = risk_exposure)
 
 # change it to raster format
 RPP = rasterFromXYZ(PPP)

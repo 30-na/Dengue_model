918ca13415d3bbfea94c6e74addfe301952433f0
sina mokhtar <sinamokhtar@gmail.com>
2023-07-06 13:35:15 -0600

    use resahpe2 function not complete

--- Functions/load_cn_data.R
+++ Functions/load_cn_data.R
@@ -2,95 +2,112 @@
 library(ncdf4)
 library(RColorBrewer)
 library(lattice)
-
-
-# set path and filename
-ncpath <- "Data/"
-ncname <- "Complete_TAVG_LatLong1.nc"  
-ncfname <- paste(ncpath, ncname, sep="")
-
-
-
-
+library(dplyr)
+library(ggplot2)
+library(reshape2)
 
 # open a netCDF file
-ncin <- nc_open(ncfname)
-print(ncin)
+nc_precip <- nc_open("Data/cru_ts4.06.1901.2021.pre.dat.nc")
+print(nc_precip)
 
 
 # set the variable names
-names(ncin$var)
-dname <- "temperature"  # note: tmp means temperature (not temporary)
-names(ncin$dim)
-# x = "longitude"
-# y = "latitude"
-
-x = "longitude"
-y = "latitude"
-
-
+names(nc_precip$var)
+names(nc_precip$dim)
+x = "lon"
+y = "lat"
 t = "time"
 
 
-
-
 # get longitude
-lon <- sort(ncvar_get(ncin,x))
-nlon <- dim(lon)
-head(lon)
+lon <- sort(ncvar_get(nc_precip,x))
 
 # get latitude
-lat <- sort(ncvar_get(ncin,y))
-nlat <- dim(lat)
-head(lat)
-print(c(nlon,nlat))
+lat <- sort(ncvar_get(nc_precip,y))
 
 # get time
-time <- ncvar_get(ncin,t)
-time
+time <- ncvar_get(nc_precip,t)
+
+# Convert numeric time values to date format
+date <- as.Date("1900-01-01") + (time - 1)
+
+# Print the converted date values Jan. 1901- Dec. 2021)
+range(date)
+
+# get precipitation
+pre_array <- ncvar_get(nc_precip, "pre")
+
+# get variables
+fullname <- ncatt_get(nc_precip,"pre","long_name")
+dunits <- ncatt_get(nc_precip,"pre","units")
+fillvalue <- ncatt_get(nc_precip,"pre","_FillValue")
+dim(pre_array)
 
 
-tunits <- ncatt_get(ncin,"time","units")
-nt <- dim(time)
-nt
+# get global attributes
+title <- ncatt_get(nc_precip,0,"title")
+institution <- ncatt_get(nc_precip,0,"institution")
+datasource <- ncatt_get(nc_precip,0,"source")
+references <- ncatt_get(nc_precip,0,"references")
+history <- ncatt_get(nc_precip,0,"history")
+Conventions <- ncatt_get(nc_precip,0,"Conventions")
 
-# get temperature
-tmp_array <- ncvar_get(ncin,dname)
-dlname <- ncatt_get(ncin,dname,"long_name")
-dunits <- ncatt_get(ncin,dname,"units")
-fillvalue <- ncatt_get(ncin,dname,"_FillValue")
-dim(tmp_array)
-
-
-# # get global attributes
-title <- ncatt_get(ncin,0,"title")
-institution <- ncatt_get(ncin,0,"institution")
-datasource <- ncatt_get(ncin,0,"source")
-references <- ncatt_get(ncin,0,"references")
-history <- ncatt_get(ncin,0,"history")
-Conventions <- ncatt_get(ncin,0,"Conventions")
-# 
-# # close the file
-# nc_close(ncin)
-# 
-
-# # Convert time to year and month and date format
-# year <- floor(time)
-# month <- round(((time - year) * 12) + .5, 0)
-# date = as.Date(paste(year, month, "15", sep="-"))
+ 
+# close the file
+ nc_close(nc_precip)
 
 
 # replace netCDF fill values with NA's
-# tmp_array[tmp_array==fillvalue$value] <- NA
-# length(na.omit(as.vector(tmp_array[,,1])))
+ pre_array[pre_array==fillvalue$value] <- NA
 
-# get a single slice or layer (January)
-m <- 2900
-tmp_slice <- tmp_array[,,m]
+
+# # get a single slice or layer (January)
+# 
+# m=1000
+# tmp_slice <- tmp_array[,,m]
 
 # quick map
-#png("my_plot.png")
-#image(lon,lat,tmp_slice, col=rev(brewer.pal(10,"RdBu")), ylim = rev(range(lat)))
-image(lon,lat,tmp_slice, col=brewer.pal(10,"RdBu"))
-#dev.off()
+# image(lon,lat,tmp_slice, col=brewer.pal(10,"RdBu"))
+
+# get dataframe
+# df1 = expand.grid( Longitude = 1:dim(tmp_array)[1],
+#                   Latitude = 1:dim(tmp_array)[2]) %>%
+#     dplyr::mutate(Precipitation = c(tmp_array[ , ,1]))
+# 
+
+# Plotting the dataframe using ggplot
+# ggplot(df, aes(x = Longitude, y = Latitude, fill = Precipitation)) +
+#   geom_tile() +
+#   scale_fill_gradient(low = "white", high = "blue") +
+#   labs(x = "Longitude", y = "Latitude", title = "Precipitation") +
+#   theme_minimal()+
+#     facet_grid(~Date)
+
+
+# df = data.frame(matrix(tmp_array, nrow=dim(tmp_array)[3], byrow=TRUE))
+# 
+# a = tmp_array[,,5]
+# sum(!is.na(a))
+# 
+# 
+date
+
+# Reshape the array to a long format
+df <- melt(pre_array)
+
+# Rename the columns
+colnames(df) <- c("Longitude", "Latitude", "Date", "Precipitation")
+
+# add date
+tempData = df %>%
+    mutate(Date1 = rep(date, each = (dim(pre_array)[1] * dim(pre_array)[2])))
+
+
+
+rep(1:3, each=4)
+
+
+
+
+
 

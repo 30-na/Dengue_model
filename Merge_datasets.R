995e5dadb65d7ad74a869b3c8b54ce3b84a85b8c
sina mokhtar <sinamokhtar@gmail.com>
2023-07-09 22:27:43 -0600

    add aegypti dataset

--- Merge_datasets.R
+++ Merge_datasets.R
@@ -4,9 +4,19 @@ library("dplyr")
 load("processedData/CEDA_temp_data.rda")
 load("processedData/CEDA_precip_data.rda")
 load("processedData/RiskOfExposure.rda")
-
+load("processedData/aegypti.rda")
 
 #merge all data sets
-R0_Data = temp_data %>%
-    left_join(precip_data, by = c("Longitude", "Latitude","Date")) %>%
-    left_join(Rse, by = c("Longitude", "Latitude"))
+merged_data = temp_data %>%
+    left_join(precip_data,
+              by = c("Longitude", "Latitude","Date")) %>%
+    left_join(Rse,
+              by = c("Longitude", "Latitude")) %>%
+    left_join(aegypti,
+              by = c("Longitude", "Latitude"))
+
+
+
+save(merged_data,
+    file = "processedData/merged_input_Data.rda")
+

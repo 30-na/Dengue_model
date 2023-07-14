5c4a9617834e2308d4e1c02346ba71f57d86b5c6
sina mokhtar <sinamokhtar@gmail.com>
2023-07-13 18:54:58 -0600

    add country name to dataset

--- R0_statistics.R
+++ R0_statistics.R
@@ -2,10 +2,11 @@
 # load library
 library(dplyr)
 library(ggplot2)
-
+library(geonames)
 
 # load the data
 load("processedData/R0_data.rda")
+options(geonamesUsername="30na")
 
     
 # summary statistics
@@ -23,10 +24,15 @@ df = R0_data %>%
            )
 
 R0_30years = df %>%
-    dplyr::filter(Year >= 1990 & Year <= 2020)
+    dplyr::filter(
+        Year >= 1990 & Year <= 2020
+        ) %>%
+    dplyr::mutate(
+        country = geonames::GNcountryCode(lat = Latitude , lng = Longitude)$countryName
+        )
 
 save(R0_30years,
-     file = "R0_30years.rda")
+     file = "processedData/R0_30years.rda")
 
 R0YearStat = df %>%
     group_by(Year) %>%

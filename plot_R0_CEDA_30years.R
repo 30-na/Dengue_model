0a440a4dbb4073b0c94fa535d6448c8f9d5ecdfa
sina mokhtar <sinamokhtar@gmail.com>
2023-07-24 12:46:45 -0600

    remove alternative models from plots

--- plot_R0_CEDA_30years.R
+++ plot_R0_CEDA_30years.R
@@ -51,20 +51,20 @@ print_StandardizedR0 = function(countryList, t){
             mean_temp = mean(Temperature, na.rm = T),
             mean_prec = mean(Precipitation, na.rm = T),
             mean_ber = mean(r0_briere, na.rm = T),
-            mean_quad = mean(r0_quadratic, na.rm = T),
-            mean_ber_alt = mean(r0_A_briere, na.rm = T),
-            mean_quad_alt = mean(r0_A_quadratic, na.rm = T)
+            mean_quad = mean(r0_quadratic, na.rm = T)
+            #mean_ber_alt = mean(r0_A_briere, na.rm = T),
+            #mean_quad_alt = mean(r0_A_quadratic, na.rm = T)
             ) %>%
         dplyr::mutate(
             temp_diff = mean_temp - mean(mean_temp, na.rm = T),
             prec_diff = mean_prec - mean(mean_prec, na.rm = T),
             ber_diff = mean_ber - mean(mean_ber, na.rm = T),
-            quad_diff = mean_quad - mean(mean_quad , na.rm = T),
-            ber_alt_diff = mean_ber_alt - mean(mean_ber_alt , na.rm = T),
-            quad_alt_diff = mean_quad_alt - mean(mean_quad_alt, na.rm = T)
+            quad_diff = mean_quad - mean(mean_quad , na.rm = T)
+            #ber_alt_diff = mean_ber_alt - mean(mean_ber_alt , na.rm = T),
+            #quad_alt_diff = mean_quad_alt - mean(mean_quad_alt, na.rm = T)
         ) %>%
         dplyr::select(
-            Year, temp_diff:quad_alt_diff
+            Year, temp_diff:quad_diff
         ) %>%
         reshape2::melt(
             id.vars = "Year"
@@ -76,17 +76,18 @@ print_StandardizedR0 = function(countryList, t){
                   aes(x = Year,
                       y = value,
                       fill = factor(value < 0))) +
-        geom_col(col = "gray") +
-        scale_fill_manual(values = c("FALSE" = "red", "TRUE" = "steelblue")) +
+        geom_col(col = "#4d4d4d") +
+        scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#3288bd")) +
         facet_wrap(~ variable,
                    scale = "free_y",
                    ncol = 1,
                    labeller = as_labeller(c(temp_diff = "Temperature",
                                             prec_diff = "Precipitation",
                                             ber_diff = "R0 (Briere)",
-                                            quad_diff = "R0 (Quadratic)",
-                                            ber_alt_diff = "R0 (Briere Alternative)",
-                                            quad_alt_diff = "R0 (Quadratic Alternative)"))) +
+                                            quad_diff = "R0 (Quadratic)"
+                                            #ber_alt_diff = "R0 (Briere Alternative)",
+                                            #quad_alt_diff = "R0 (Quadratic Alternative)"
+                                            ))) +
         theme_minimal() +
         labs(title = paste0("Standardized R0 anomalies with respect to 1990-2020 at ", t),
              y = "",
@@ -97,7 +98,7 @@ print_StandardizedR0 = function(countryList, t){
     # Print the plots
     ggsave(paste0("Figures/Standardized_R0_", t, ".jpg"),
            g_y,
-           height = 12, width = 8, scale = 1)
+           height = 9, width = 7, scale = 1)
     
     
 }
@@ -109,3 +110,70 @@ print_StandardizedR0(countryList = c("Vietnam"), t = "Vietnam")
 print_StandardizedR0(countryList = PAHO, t = "PAHO")
 print_StandardizedR0(countryList = SEARO, t = "SEARO")
 print_StandardizedR0(countryList = WPRO, t = "WPRO")
+
+
+
+ 
+# define the function for whole world
+print_StandardizedR0 = function(){
+    y_long = R0 %>% 
+        dplyr::group_by(
+            Year
+        ) %>%
+        dplyr::summarise(
+            mean_temp = mean(Temperature, na.rm = T),
+            mean_prec = mean(Precipitation, na.rm = T),
+            mean_ber = mean(r0_briere, na.rm = T),
+            mean_quad = mean(r0_quadratic, na.rm = T)
+            #mean_ber_alt = mean(r0_A_briere, na.rm = T),
+            #mean_quad_alt = mean(r0_A_quadratic, na.rm = T)
+        ) %>%
+        dplyr::mutate(
+            temp_diff = mean_temp - mean(mean_temp, na.rm = T),
+            prec_diff = mean_prec - mean(mean_prec, na.rm = T),
+            ber_diff = mean_ber - mean(mean_ber, na.rm = T),
+            quad_diff = mean_quad - mean(mean_quad , na.rm = T)
+            #ber_alt_diff = mean_ber_alt - mean(mean_ber_alt , na.rm = T),
+            #quad_alt_diff = mean_quad_alt - mean(mean_quad_alt, na.rm = T)
+        ) %>%
+        dplyr::select(
+            Year, temp_diff:quad_diff
+        ) %>%
+        reshape2::melt(
+            id.vars = "Year"
+        )
+    
+    
+    # Create the plot
+    g_y <- ggplot(data = y_long,
+                  aes(x = Year,
+                      y = value,
+                      fill = factor(value < 0))) +
+        geom_col(col = "#4d4d4d") +
+        scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#3288bd")) +
+        facet_wrap(~ variable,
+                   scale = "free_y",
+                   ncol = 1,
+                   labeller = as_labeller(c(temp_diff = "Temperature",
+                                            prec_diff = "Precipitation",
+                                            ber_diff = "R0 (Briere)",
+                                            quad_diff = "R0 (Quadratic)"
+                                            #ber_alt_diff = "R0 (Briere Alternative)",
+                                            #quad_alt_diff = "R0 (Quadratic Alternative)"
+                   ))) +
+        theme_minimal() +
+        labs(title = paste0("Standardized R0 anomalies with respect to 1990-2020"),
+             y = "",
+             x = "") +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 10, face = "bold")) +
+        guides(fill = FALSE)
+    
+    # Print the plots
+    ggsave(paste0("Figures/Standardized_R0.jpg"),
+           g_y,
+           height = 9, width = 7, scale = 1)
+    
+    
+}
+
+print_StandardizedR0()

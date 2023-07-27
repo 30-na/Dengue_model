1b7e22bd242220508e54d18924d38104a9f74371
sina mokhtar <sinamokhtar@gmail.com>
2023-07-26 19:16:56 -0600

    load gread country data

--- Functions/get_R0_YearMean_Map_1950to2020.R
+++ Functions/get_R0_YearMean_Map_1950to2020.R
@@ -6,26 +6,7 @@ library(sf)
 
 # load the data
 load("processedData/R0_data.rda")
-
-
-
-# Read the data from the CSV file and convert the 'geometry' column to sf object
-grid_country <- data.table::fread("processedData/custom_grid_with_country_names.csv")
-grid_country_sf <- st_as_sf(grid_country,
-                            wkt = "geometry",
-                            crs = 4326)
-
-
-
-grid_country_clean = grid_country %>%
-    dplyr::mutate(
-        Longitude = st_coordinates(grid_country_sf$geometry)[,1],
-        Latitude = st_coordinates(grid_country_sf$geometry)[,2],
-        country = ifelse(country_name == "" | country_name == "Antarctica", NA, country_name)
-    ) %>%
-    dplyr::select(Longitude,
-                  Latitude,
-                  country)
+load("processedData/grid_country_clean.rda")
 
 
 

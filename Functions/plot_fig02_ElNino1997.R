a6da0370c79c1f893cc322dc7af33bf13460c0a8
sina mokhtar <sinamokhtar@gmail.com>
2023-08-09 11:33:10 -0600

    initial commit

--- Functions/plot_fig02_ElNino1997.R
+++ Functions/plot_fig02_ElNino1997.R
@@ -6,118 +6,116 @@ library(reshape2)
 library(viridis)
 library(zoo)
 library(ggpubr)
-
+library(maps)
 
 # load data
 load("processedData/R0_1950to2020.rda")
-load("processedData/R0MeanStatMap.rda")
-load("processedData/R0MeanYearStat.rda")
+load("processedData/R0MeanMonthStatMap.rda")
 
 
+
+labels = rev(c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0","0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"))
 df_map_1997 = R0 %>%
     dplyr::filter(Date >= "1997-06-15" & Date < "1998-06-15") %>%
-    group_by(Longitude,
+    group_by(Month,
+             Longitude,
              Latitude)%>%
     dplyr::summarize(
-    mean_ber_1year = mean(r0_briere, na.rm = T),
-    mean_quad_1year = mean(r0_quadratic, na.rm = T)
+        mean_ber_ElNino = mean(r0_briere, na.rm = T),
+        mean_quad_ElNino = mean(r0_quadratic, na.rm = T),
+        mean_temp_ElNino = mean(Temperature, na.rm = T)
     ) %>%
     dplyr::left_join(
-        R0MeanStatMap , by=c("Longitude","Latitude")
+        R0MeanMonthStatMap , by=c("Longitude","Latitude", "Month")
     ) %>%
-    dplyr::select(
-        Longitude,
-        Latitude,
-        mean_ber_1year,
-        mean_quad_1year,
-        mean_ber,
-        mean_quad
-    ) %>%
-    dplyr::mutate(diff_ber = mean_ber_1year - mean_ber,
-                  diff_quad = mean_quad_1year - mean_quad,
-                  per_ber = ifelse(diff_ber == 0 & mean_ber ==0, 0, (diff_ber / mean_ber)*100),
-                  per_quad = ifelse(diff_quad == 0 & mean_quad ==0, 0, (diff_quad / mean_quad)*100),
-                  mean_ber_disc = cut(per_ber,
-                                      breaks = c(-Inf,-40, -30, -20, -10, 0, 10, 20, 30, 40, Inf),
-                                      labels = c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"),
-                                      include.lowest = TRUE),
-                  mean_quad_disc = cut(per_quad,
-                                       breaks = c(-Inf,-40, -30, -20, -10, 0, 10, 20, 30, 40, Inf),
-                                       labels = c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"),
-                                       include.lowest = TRUE))
-
-
-
-
-
-df_mean = R0MeanYearStat %>% 
-    dplyr::mutate(
-        total_mean_ber = mean(mean_ber, na.rm = T),
-        total_mean_quad = mean(mean_quad, na.rm = T),
-        ber_diff = mean_ber - total_mean_ber,
-        quad_diff = mean_quad - total_mean_quad,
-        ber_per = (ber_diff / total_mean_ber)*100,
-        quad_per = (quad_diff / total_mean_quad)*100,
-        ber_per_smooth = rollmean(ber_per,
-                                  k = 5,
-                                  fill = NA,
-                                  align = "center"),
-        quad_per_smooth = rollmean(quad_per,
-                                   k = 5,
-                                   fill = NA,
-                                   align = "center")
+    dplyr::mutate(diff_ber = mean_ber_ElNino - mean_ber,
+                  diff_quad = mean_quad_ElNino - mean_quad,
+                  diff_temp = mean_temp_ElNino - mean_temp,
+                  per_ber = ifelse(diff_ber == 0 & mean_ber == 0, 0, (diff_ber / mean_ber)*100),
+                  per_quad = ifelse(diff_quad == 0 & mean_quad == 0, 0, (diff_quad / mean_quad)*100),
+                  per_temp = ifelse(diff_temp== 0 & mean_temp == 0, 0, (diff_temp / mean_temp)*100),
+                  mean_ber_temper = cut(per_ber,
+                                        breaks = c(-Inf,-40, -30, -20, -10, 0, 10, 20, 30, 40, Inf),
+                                        labels = c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"),
+                                        include.lowest = TRUE),
+                  mean_quad_temper = cut(per_quad,
+                                         breaks = c(-Inf,-40, -30, -20, -10, 0, 10, 20, 30, 40, Inf),
+                                         labels = c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"),
+                                         include.lowest = TRUE),
+                  mean_quad_temper = cut(per_quad,
+                                         breaks = c(-Inf,-40, -30, -20, -10, 0, 10, 20, 30, 40, Inf),
+                                         labels = c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"),
+                                         include.lowest = TRUE),
+                  mean_temp_temper = cut(per_temp,
+                                         breaks = c(-Inf,-40, -30, -20, -10, 0, 10, 20, 30, 40, Inf),
+                                         labels = c("< -40", "-40 to -30", "-30 to -20", "-20 to -10", "-10 to 0", "0 to 10", "10 to 20", "20 to 30", "30 to 40", "> 40"),
+                                         include.lowest = TRUE),
+                  mean_ber_disc = factor(ifelse(per_ber  == 0, "0", as.character(mean_ber_temper)),
+                                         levels = labels),
+                  mean_quad_disc = factor(ifelse(per_quad  == 0, "0", as.character(mean_quad_temper)),
+                                          levels =labels),
+                  mean_temp_disc = factor(ifelse(per_temp  == 0, "0", as.character(mean_temp_temper)),
+                                          levels =labels)
     )
 
 
 
 
-plot_Fig2 = function(title1, title2, fun){
-    
-    custom_colors = rev(c('#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061'))
-    
-    
-    # set Function
+plot_map = function(month, fun, title1, title2, id, cnt){
     if(fun == "berier"){
-        df_map_1997 = df_map_1997 %>%
-            dplyr::rename("var" = mean_ber_disc)
-        df_mean = df_mean %>%
-            dplyr::rename("var" = ber_per)
-        mainTitle = "Fig .2"
-        hjust = 6.3
-    } 
+        temp_df = df_map_1997 %>%
+            dplyr::rename(
+                "var1" = mean_ber_disc,
+                "var2" = per_temp
+            )
+    }
     
     if(fun == "quadratic"){
-        df_map_1997 = df_map_1997 %>%
-            dplyr::rename("var" = mean_quad_disc)
-        df_mean = df_mean %>%
-            dplyr::rename("var" = quad_per)
-        mainTitle = "Fig .2 (Quadratic)"
-        hjust = 2.03
-    } 
+        temp_df = df_map_1997 %>%
+            dplyr::rename(
+                "var1" = mean_quad_disc,
+                "var2" = per_temp
+            )
+    }
+    
+    temp_df = temp_df %>%
+        dplyr::filter(
+            Month == month,
+            country == cnt
+        )
+    
+    custom_colors = c('#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#e0e0e0','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061')
+    
+    
     
     g1 = ggplot(
-        df_map_1997,
-        aes(x = Longitude, y = Latitude, color = var)
+        temp_df,
+        aes(x = Longitude, y = Latitude, fill = var1)
     ) +
-        geom_point(size=.1) +
-        borders(
-            size=.2,
-            colour = "black",
-            xlim=c(-180, 180),
-            ylim=c(-62, 90)
-        )+ 
+        geom_tile() +
+        geom_polygon(data = map_data("world", region = cnt),
+                     aes(x = long, y = lat, group = group),
+                     color = "black",
+                     fill = NA,
+                     size = 1) +
+        # borders(
+        #     size=.2,
+        #     colour = "black",
+        #     xlim = c(115, 127),  # Adjust these values to focus on the Philippines
+        #     ylim = c(4, 21)
+        # )+
         #scale_color_viridis()+
-        scale_color_manual(values = custom_colors,
-                           na.value = "transparent",
-                           drop = TRUE,
-                           na.translate = F) +
+        scale_fill_manual(values = custom_colors,
+                          na.value = "transparent",
+                          drop = TRUE,
+                          na.translate = F) +
         labs(
-            #title = title1,
+            #title = paste0(month, title),
             x = "",
             y = "",
-            color = "R0 (%)"
+            fill = "R0 (%)"
         )+
-        ylim(c(-62, 90)) +
+        #ylim(c(-62, 90)) +
         
         theme_minimal()+
         guides(color = guide_legend(override.aes = list(size = 8, shape=15)))+
@@ -125,32 +123,52 @@ plot_Fig2 = function(title1, title2, fun){
             axis.text.x = element_blank(),
             axis.text.y = element_blank(),
             plot.title = element_text(size = 12)
-        )
+        )+
+        coord_fixed()
     
     
-    g2 <- ggplot(data = df_mean,
-                 aes(x = as.numeric(Year),
-                     y = var)) +  # Remove fill aesthetic from here
-        geom_col(aes(fill = factor(var < 0)), col = "#4d4d4d") +  # Specify fill for geom_col
-        geom_line(aes(y = ber_per_smooth),
-                  col = "black",
-                  size = 1) +
-        scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#4393c3")) +
-        theme_minimal() +
-        labs(#title = title2,
-            y = "Standardized R0 anomalies (%)",
-            x = "") +
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 8, face = "bold")) +
-        guides(fill = FALSE) +
-        scale_x_continuous(
-            breaks = seq(1950, 2020, by = 5),
-            labels = seq(1950, 2020, by = 5))
+    
+    
+    g2 = ggplot(
+        temp_df,
+        aes(x = Longitude, y = Latitude, fill = var2)
+    ) +
+        geom_tile() + 
+        geom_polygon(data = map_data("world", region = cnt), aes(x = long, y = lat, group = group),
+                     color = "black", fill = NA, size = 1) +
+        # borders(
+        #     size=.2,
+        #     colour = "black",
+        #     xlim = c(115, 127),  # Adjust these values to focus on the Philippines
+        #     ylim = c(4, 21)
+        # )+ 
+        #scale_color_viridis()+
+        scale_fill_gradientn(colors = rev(custom_colors),
+                             #limits = c(-15, 15),
+                             limits = c(-15, 15),
+                             na.value = "transparent") +
+        labs(
+            #title = paste0(month, title),
+            x = "",
+            y = "",
+            fill = "Temperature %"
+        )+
+        #ylim(c(-62, 90)) +
+        
+        theme_minimal()+
+        #guides(color = guide_legend(override.aes = list(size = 8, shape=15)))+
+        theme(
+            axis.text.x = element_blank(),
+            axis.text.y = element_blank(),
+            plot.title = element_text(size = 12)
+        )+
+        coord_fixed()
     
     
     g <- ggarrange(g1,
                    g2,
-                   labels=c(title1,
-                            title2),
+                   labels=c(paste0(month, title1, cnt),
+                            paste0(month, title2, cnt)),
                    font.label = list(size = 12,
                                      color = "black",
                                      face = "plain"),
@@ -158,22 +176,35 @@ plot_Fig2 = function(title1, title2, fun){
                    nrow = 2,
                    hjust = -.02)
     
-    g = annotate_figure(g,
-                        top = text_grob(mainTitle,
-                                        #face = "bold",
-                                        size = 20,
-                                        hjust=hjust)
-    )
-    
-    ggsave(paste0("Figures/fig2_", fun, "1997.jpg"),
+    ggsave(paste0("Figures/month/country/1997/", sprintf("%02d", id),cnt ,month, "_map_month_",fun, ".jpg"),
            g,
-           height=9,width=9,scale=1)
+           height=9,width=5,scale=1.6)
 }
 
-plot_Fig2(title1 = "(A) Annual R0 1997-98 El Nino anomaly (percentage) with respect to the 1950–2020 period",
-          title2 = "(B) Standardized R0 anomalies with respect to the 1950–2020 period",
-          fun = "berier")
 
-plot_Fig2(title1 = "(A) Annual R0 1997-98 El Nino anomaly (percentage) with respect to the 1950–2020 period",
-          title2 = "(B) Standardized R0 anomalies with respect to the 1950–2020 period",
-          fun = "quadratic")
+months <- c("February", "March", "April", "May", "June", 
+            "July", "August", "September", "October", "November", "December", "January" )
+
+
+for(m in 1:length(months)){
+    
+    plot_map(month = months[m],
+             fun = "berier",
+             title1 = " R0 1997-98 El Nino ",
+             title2 = " 1997-98 El Nino Temperature differnce with respect to the same month average 1950–2020 ",
+             id = m,
+             cnt = "Philippines"
+    )
+}
+
+for(m in 1:length(months)){
+    
+    plot_map(month = months[m],
+             fun = "berier",
+             title1 = " R0 1997-98 El Nino ",
+             title2 = " 1997-98 El Nino Temperature differnce with respect to the same month average 1950–2020 ",
+             id = m,
+             cnt = "Brazil"
+    )
+}
+

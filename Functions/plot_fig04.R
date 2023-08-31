8c2cc3788d08f7b128dfc4542b5e204e33837ccd
sina mokhtar <sinamokhtar@gmail.com>
2023-08-31 09:53:44 -0600

    add plot 04

--- Functions/plot_fig04.R
+++ Functions/plot_fig04.R
@@ -45,7 +45,7 @@ data = R0MeanYearStat %>%
         quad_per = (quad_diff / total_mean_quad)*100,
         temp_per = (temp_diff / total_mean_temp)*100,
         prec_per = (prec_diff / total_mean_prec)*100
-        ) %>%
+        )%>%
     dplyr::select(
         Year,
         ber_per,
@@ -70,41 +70,28 @@ data = R0MeanYearStat %>%
     )
     
 
-plot_Fig2 = function(fun){
-    
-    # set Function
-    if(fun == "berier"){
-        df = data %>%
-            dplyr::filter(
-                variable %in% c("ber_per",
-                                "temp_per",
-                                "prec_per",
-                                "ber_per_NoPrecip")
-            )
-    } 
-    
-    if(fun == "quadratic"){
-        df = data %>%
-            dplyr::filter(
-                variable %in% c("quad_per",
-                                "temp_per",
-                                "prec_per",
-                                "quad_per_NoPrecip")
-            )
-    } 
-    
-    
-    g <- ggplot(data = df,
+plot_Fig4a = function(title1, title2, title3, title4){
+
+    # G1
+    g1 <- ggplot(data = data %>%
+                     dplyr::filter(
+                         variable %in% c("ber_per",
+                                         #"temp_per",
+                                         #"prec_per",
+                                         "ber_per_NoPrecip"))%>%
+                     mutate(variable = factor(variable)),
                  aes(x = as.numeric(Year),
-                     y = value,
-                     fill = variable)) +  
-        geom_bar(stat = "identity", position = "dodge", alpha=.8, width = 0.85, col="black", size=.02) +  
-        geom_line(aes(y = smooth_value,
+                     y = value)) +  
+        geom_bar(aes(fill = variable),
+                 stat = "identity", position = "dodge", alpha=.5, width = 0.75, col="black", size=.02) +  
+        geom_line(aes(y = smooth_value ,
                       col = variable),
-                  size = 1) +
+                  size = 1,
+                  show.legend=F) +
+        
         #scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#4393c3")) +
         theme_minimal() +
-        labs(title = paste("Fig. S3) Impact of Precipitation on", fun, "Model"),
+        labs(title = title1,
             y = "Standardized R0 anomalies (%)",
             x = "") +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 8, face = "bold"),
@@ -115,19 +102,209 @@ plot_Fig2 = function(fun){
             labels = seq(1950, 2020, by = 5))+
         scale_fill_brewer(
             palette = "Dark2",
+            labels = c("With Precipitaion", "Without Precipitation"),
             guide = guide_legend(
-                title = ""
-            )
+                title = "")
+            )+
+        
+        scale_color_brewer(palette = "Dark2",
+                           
+                           #labels = c("With Precipitaion", "Without Precipitation")
+                           ) +
+        guides(
+            col = guide_legend(title = NULL),
+            
+        )
+    
+    
+    
+    
+    # G2
+    g2 <- ggplot(data = data %>%
+                     dplyr::filter(
+                         variable %in% c("quad_per",
+                                         #"temp_per",
+                                         #"prec_per",
+                                         "quad_per_NoPrecip")
+                     )%>%
+                     mutate(variable = factor(variable)),
+                 aes(x = as.numeric(Year),
+                     y = value)) +  
+        geom_bar(aes(fill = variable),
+                 stat = "identity",
+                 position = "dodge",
+                 alpha=.5,
+                 width = 0.75,
+                 col="black",
+                 size=.02,
+                 show.legend=F) +  
+        geom_line(aes(y = smooth_value ,
+                      col = variable),
+                  size = 1,
+                  show.legend=F) +
+        
+        #scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#4393c3")) +
+        theme_minimal() +
+        labs(title = title2,
+             y = "Standardized R0 anomalies (%)",
+             x = "") +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 8, face = "bold"),
+              legend.position = "bottom") +
+        
+        scale_x_continuous(
+            breaks = seq(1950, 2020, by = 5),
+            labels = seq(1950, 2020, by = 5))+
+        scale_fill_brewer(
+            palette = "Dark2",
+            labels = c("With Precipitaion", "Without Precipitation"),
+            guide = guide_legend(
+                title = "")
         )+
-        scale_color_brewer(palette = "Dark2") +
-        guides()
+        
+        scale_color_brewer(palette = "Dark2",
+                           
+                           #labels = c("With Precipitaion", "Without Precipitation")
+        ) +
+        guides(
+            col = guide_legend(title = NULL),
+            
+        )
+    
+    
+    p = "#ef3b2c"
+    
+    # G3
+    g3 <- ggplot(data = data %>%
+                     dplyr::filter(
+                         variable %in% c("temp_per")
+                     )%>%
+                     mutate(variable = factor(variable)),
+                 aes(x = as.numeric(Year),
+                     y = value)) +  
+        geom_bar(aes(fill = variable),
+                 stat = "identity",
+                 position = "dodge",
+                 alpha=.7,
+                 width = 0.75,
+                 col="black",
+                 size=.02,
+                 show.legend=F,
+                 fill = p ) +  
+        geom_line(aes(y = smooth_value),
+                  size = 1,
+                  show.legend=F,
+                  col = "black") +
+        
+        #scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#4393c3")) +
+        theme_minimal() +
+        labs(title = title3,
+             y = "Standardized Temperature anomalies (%)",
+             x = "") +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 8, face = "bold"),
+              legend.position = "bottom") +
+        
+        scale_x_continuous(
+            breaks = seq(1950, 2020, by = 5),
+            labels = seq(1950, 2020, by = 5))+
+        scale_fill_brewer(
+            palette = p,
+            #labels = c("With Precipitaion", "Without Precipitation"),
+            guide = guide_legend(
+                title = "")
+        )+
+        
+        guides(
+            col = guide_legend(title = NULL),
+            
+        )
+    
+    
+    
+    
+    
+    
+    p = "#1d91c0"
+    
+    # G4
+    g4 <- ggplot(data = data %>%
+                     dplyr::filter(
+                         variable %in% c("prec_per")
+                     )%>%
+                     mutate(variable = factor(variable)),
+                 aes(x = as.numeric(Year),
+                     y = value)) +  
+        geom_bar(aes(fill = variable),
+                 stat = "identity",
+                 position = "dodge",
+                 alpha=.7,
+                 width = 0.75,
+                 col="black",
+                 size=.02,
+                 show.legend=F,
+                 fill = p ) +  
+        geom_line(aes(y = smooth_value ,
+                      col = p),
+                  size = 1,
+                  show.legend=F,
+                  col="black") +
+        
+        #scale_fill_manual(values = c("FALSE" = "#d6604d", "TRUE" = "#4393c3")) +
+        theme_minimal() +
+        labs(title = title4,
+             y = "Standardized Precipitation anomalies (%)",
+             x = "") +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 8, face = "bold"),
+              legend.position = "bottom") +
+        
+        scale_x_continuous(
+            breaks = seq(1950, 2020, by = 5),
+            labels = seq(1950, 2020, by = 5))+
+        scale_fill_brewer(
+            palette = p,
+            #labels = c("With Precipitaion", "Without Precipitation"),
+            guide = guide_legend(
+                title = "")
+        )+
+        
+        guides(
+            col = guide_legend(title = NULL),
+            
+        )
+    
+    
+    
+    
+    g <- ggarrange(g1,
+                   g2,
+                   g3,
+                   g4,
+                   heights = c(1.1,1,1,1),
+                   font.label = list(size = 12,
+                                     color = "black",
+                                     face = "plain"),
+                   ncol = 1,
+                   nrow = 4,
+                   hjust = -.02)
+    
+    g = annotate_figure(g,
+                        top = text_grob("FIG 04",
+                                        #face = "bold",
+                                        size = 20,
+                                        hjust=7)
+    )
     
 
-    ggsave(paste0("Figures/fig3s", fun, ".jpg"),
+    ggsave(paste0("Figures/fig4.jpg"),
            g,
-           height=6,width=16,scale=1)
+           height=16,width=12,scale=1)
 }
 
-plot_Fig2(fun = "berier")
-plot_Fig2(fun = "quadratic")
+plot_Fig4a(
+    title1 = "A) Compare the model incorporating precipitation with the model excluding precipitation using the Briere function",
+    title2 = "B) Compare the model incorporating precipitation with the model excluding precipitation using the Quadratic function",
+    title3 = "C) Standardized Temperature anomalies with respect to the 1950-2020 period",
+    title4 = "D) Standardized Precipitation anomalies with respect to the 1950-2020 period"
+    )
+
+           
 
